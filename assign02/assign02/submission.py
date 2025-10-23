75d2abe371ad61c1310848e863bba22dadf6b1ce
Nemo <ejongwon7@gmail.com>
2025-10-23 20:11:27 +0900

    Feat: implements 1b

--- assign02/assign02/submission.py
+++ assign02/assign02/submission.py
@@ -31,6 +31,7 @@ def get_conditional_prob1(delta: float, epsilon: float, eta: float, c2: int, d2:
 
     return P_d2[c2] / normalizer
 
+
 def get_conditional_prob2(delta, epsilon, eta, c2, d2, d3):
     """
     :param delta: [δ] is the parameter governing the distribution of the initial car's position
@@ -42,13 +43,23 @@ def get_conditional_prob2(delta, epsilon, eta, c2, d2, d3):
 
     :returns: a number between 0~1 corresponding to P(C_2=c2 | D_2=d2, D_3=d3)
     """
-    # Problem 1b
-    # BEGIN_YOUR_ANSWER (our solution is 17 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
-    # END_YOUR_ANSWER
+    DOMAIN = 0, 1
+
+    P_c1 = delta, 1 - delta
+    P_c_given_cp = lambda c, cp: epsilon if c != cp else 1 - epsilon
+    P_d_given_c = lambda d, c: eta if d != c else 1 - eta
+
+    P_c2 = tuple(map(lambda c2: sum(P_c1[c1] * P_c_given_cp(c2, c1) for c1 in DOMAIN), DOMAIN))
+    P_d2_given_c2 = tuple(map(P_d_given_c, (d2, d2), DOMAIN))
+    P_d3_given_c2 = tuple(map(lambda c2: sum(P_c_given_cp(c3, c2) * P_d_given_c(d3, c3) for c3 in DOMAIN), DOMAIN))
+    
+    P_d2d3_given_c2 = tuple(map(math.prod, zip(P_d2_given_c2, P_d3_given_c2)))
+    P_d2d3 = tuple(map(math.prod, zip(P_c2, P_d2d3_given_c2)))
+    normalizer = sum(P_d2d3)
+
+    return P_d2d3[c2] / normalizer
 
 
-# Problem 1c
 def get_epsilon():
     """
     return a value of epsilon (ε)
